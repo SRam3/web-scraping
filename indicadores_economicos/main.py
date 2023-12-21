3a8127f78c1b6d91fabbc9a7020e4483366a0bb1
Sebastian Ramirez Ramirez <sebastian.ramirez@dataknow.co>
2023-12-21 08:57:27 -0500

    changes

--- indicadores_economicos/main.py
+++ indicadores_economicos/main.py
@@ -1,5 +1,5 @@
 from config_reader import ConfigReader
-from web_scraper import WebScraper
+from web_scraper import Spider, WebProperties, HumanBehaviorSimulator, PIB
 from playwright.sync_api import sync_playwright
 
 
@@ -9,7 +9,14 @@ def main():
     pages_config = config_reader.read_config()
 
     with sync_playwright() as playwright:
-        scraper = WebScraper(playwright.chromium)
+        web_properties = WebProperties(browser_type=playwright.chromium)
+        behavior_simulator = HumanBehaviorSimulator()
+
+        pib_scraper = PIB(web_properties, behavior_simulator)
+
+        for page in pages_config:
+            if page['name'] == 'PIB':
+                scraped_data = pib_scraper.scrape(page['url'], page['selectors'])
         
 
 if __name__ == "__main__":

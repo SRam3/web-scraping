f5c120cecec1919209fb353efaa48404a8fd64e6
Sebastian Ramirez Ramirez <sebastian.ramirez@dataknow.co>
2023-12-22 11:56:59 -0500

    downloads method implemented

--- indicadores_economicos/main.py
+++ indicadores_economicos/main.py
@@ -1,5 +1,5 @@
 from config_reader import ConfigReader
-from web_scraper import Spider, WebProperties, HumanBehaviorSimulator, PIB
+from web_scraper import Spider, WebProperties, PIB
 from playwright.sync_api import sync_playwright
 
 
@@ -9,14 +9,12 @@ def main():
     pages_config = config_reader.read_config()
 
     with sync_playwright() as playwright:
-        web_properties = WebProperties(browser_type=playwright.chromium)
-        behavior_simulator = HumanBehaviorSimulator()
+        web_properties = WebProperties(playwright, browser_type=playwright.chromium)
 
-        pib_scraper = PIB(web_properties, behavior_simulator)
+        pib_config = pages_config[0]
 
-        for page in pages_config:
-            if page['name'] == 'PIB':
-                scraped_data = pib_scraper.scrape(page['url'], page['selectors'])
+        pib_scraper = PIB(web_properties)
+        file_path = pib_scraper.scrape(pib_config['url'], pib_config['selectors'])
         
 
 if __name__ == "__main__":
--- indicadores_economicos/web_scraper.py
+++ indicadores_economicos/web_scraper.py
@@ -1,42 +1,21 @@
 from playwright.sync_api import sync_playwright, BrowserContext
 from playwright_stealth import stealth_sync
-import random
+import os
 import time
+import shutil
+
 
 
 class WebProperties:
-    def __init__(self, viewport_size=None, browser_type=None):
+    def __init__(self, playwright_instance, viewport_size=None, browser_type=None, enable_stealth=True):
+        self.playwright_instance = playwright_instance
         self.viewport_size = viewport_size
         self.browser_type = browser_type if browser_type else sync_playwright().chromium
+        self.enable_stealth = enable_stealth
 
-    def apply_properties(self, page):
-        if self.viewport_size:
-            page.set_viewport_size(self.viewport_size)
-
-
-class HumanBehaviorSimulator:
-    def apply_stealth(self, browser_context, page):
-        stealth_sync(page)
-        user_agents = [
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.864.48 Safari/537.36 Edg/91.0.864.48",
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
-        ]
-
-        # Introduce delay on each network request to mimic human browsing speed
-        #page.on("request", lambda request: time.sleep(random.uniform(0.5, 1.5)))
-
-    def mimic_human_interaction(self, page):
-        # Simulate random mouse movements
-        viewport_size = page.viewport_size
-        width, height = viewport_size["width"], viewport_size["height"]
-        for _ in range(random.randint(3, 5)):
-            x, y = random.randint(0, width), random.randint(0, height)
-            page.mouse.move(x, y)
-            time.sleep(random.uniform(0.5, 1.5))
+    def apply_stealth(self, browser_context):
+        if self.enable_stealth:
+            stealth_sync(browser_context)
 
 
 class Spider:
@@ -44,24 +23,20 @@ class Spider:
         self.web_properties = web_properties
         self.browser_context = None
 
-    def start_browser(self, user_agent=None):
-        self.browser_context = self.web_properties.browser_type.launch(headless=True)
+    def start_browser(self, enable_downloads=False):
+        browser = self.web_properties.browser_type.launch(headless=True)
 
-        context_options = {}
-        if user_agent:
-            context_options['userAgent'] = user_agent
+        if enable_downloads:
+            context = browser.new_context(accept_downloads=True)
+        else:
+            context = browser.new_context()
 
-        # Create a new context with the specified user agent
-        self.browser_context = self.browser_context.new_context(**context_options)
+        page = context.new_page()
+        if self.web_properties.viewport_size:
+            page.set_viewport_size(self.web_properties.viewport_size)
+        self.web_properties.apply_stealth(context)
 
-        self.browser_context.add_init_script("""
-            Object.defineProperty(navigator, 'webdriver', {
-                get: () => undefined,
-            });
-        """)
-
-        page = self.browser_context.new_page()
-        self.web_properties.apply_properties(page)
+        self.browser_context = context
         return page
 
     def close_browser(self):
@@ -70,18 +45,46 @@ class Spider:
 
 
 class PIB(Spider):
-    def __init__(self, web_properties, behavior_simulator):
+    def __init__(self, web_properties):
         super().__init__(web_properties)
-        self.behavior_simulator = behavior_simulator
-
+    
     def scrape(self, url, selectors):
-        page = self.start_browser()
-        self.behavior_simulator.apply_stealth(self.browser_context, page)
-        self.behavior_simulator.mimic_human_interaction(page)
+        download_folder = "downloads"
+        os.makedirs(download_folder, exist_ok=True)
+
+        page = self.start_browser(enable_downloads=True)
 
         page.goto(url)
-        data_links_selector = selectors.get('data_links')
-        page.query_selector_all(data_links_selector)
+        page.wait_for_selector(selectors['data_links'])
 
+        with page.expect_download() as download_info:
+            page.click(selectors['data_links'])
+        download = download_info.value
+
+        download_path = download.path()
+        while not download_path:
+            time.sleep(1)
+            download_path = download.path()
+
+        with open(download_path, 'rb') as file:
+            file_bytes = file.read()
+
+        # Define the final path with a meaningful name
+        file_name = "CTASTRIM124.xlsx"
+        final_path = os.path.join(download_folder, file_name)
+
+        os.rename(download_path, final_path)
 
         self.close_browser()
+
+        return file_bytes, final_path
+
+        
+
+
+
+
+
+
+
+
